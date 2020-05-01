133b9172294f318e9a4f7cdeceef87c30a1c6f7b
angrynajam <najamiftikhar2020@gmail.com>
2020-05-01 11:22:47 +0500

    implemented using class

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello my name is Najam</p>
-                <a
-                    className="App-link"
-                    href="https://facebook.com/angrynajam"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    facebook
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Hello my name is Najam</p>
+                    <a
+                        className="App-link"
+                        href="https://facebook.com/angrynajam"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        facebook
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
